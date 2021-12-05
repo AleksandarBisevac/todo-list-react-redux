516d293d73e4ec1f0e36cb509368f35d357d343e
AleksandarBisevac <alek.bisevac@gmail.com>
2021-12-05 19:33:44 +0100

    first commit- added babel, npm, webpack, and made initial boilerplate for the react app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './App.css';
+
+const App = () => {
+  return <div className='App'></div>;
+};
+
+export default hot(module)(App);
